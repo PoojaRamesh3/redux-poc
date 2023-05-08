25d37be7371fb91a4fa94c04959f4e56517dd2a5
Pooja R <pooja.ramesh@imaginelearning.com>
2023-05-08 15:11:11 +0530

    Trying Responsive

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { sideBarLists } from "../config/menuObj";
+import { Link } from "react-router-dom";
 
 const Hamburger = () => {
-  return <div></div>;
+  return (
+    <ul className="flex-col flex items-start">
+      {sideBarLists.map((item, index) => (
+        <li
+          className="flex flex-row items-center justify-start hover:bg-white hover:text-custblue hover:rounded-s-[50px] px-6"
+          key={index}
+          style={{ width: "210.5px" }}
+        >
+          {item.icon}
+          <Link to={item.url} className="p-5 font-semibold">
+            {item.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Hamburger;
